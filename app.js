1465d12e54fcb4581cf22ae70f7639077bb30b9c
mira-kine <mirak0419@gmail.com>
2021-09-29 12:01:36 -0700

    made interactive name tag, adding color elements

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // import functions and grab DOM elements
+const nameInput = document.getElementById('name');
+const button = document.getElementById('generate');
+const firstName = document.getElementById('first-name');
+const pink = document.getElementById('pink');
+const skyBlue = document.getElementById('sky-blue');
+const green = document.getElementById('green');
 
 // initialize global state
 
@@ -6,3 +12,10 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+  button.addEventListener('click', ()=>{
+    let newName = nameInput.value;
+    firstName.textContent = newName;
+  
+  });
+
