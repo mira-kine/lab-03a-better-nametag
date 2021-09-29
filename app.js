066e5909d109e869a2f954c981f36004f999a005
mira-kine <mirak0419@gmail.com>
2021-09-29 14:39:00 -0700

    added pronoun button, color options

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 const nameInput = document.getElementById('name');
 const button = document.getElementById('generate');
 const firstName = document.getElementById('first-name');
-const pink = document.getElementById('pink');
-const skyBlue = document.getElementById('sky-blue');
-const green = document.getElementById('green');
+const button1 = document.getElementById('pink');
+const button2 = document.getElementById('skyBlue');
+const button3 = document.getElementById('green');
+const pronounInput = document.getElementById('pronoun')
+const button4 = document.getElementById('input');
+const myPronoun = document.getElementById('myPronoun');
+let count = 0;
 
 // initialize global state
 
@@ -16,6 +20,24 @@ const green = document.getElementById('green');
   button.addEventListener('click', ()=>{
     let newName = nameInput.value;
     firstName.textContent = newName;
-  
   });
 
+  button1.addEventListener('click', ()=>{
+    document.getElementById('header').style.backgroundColor = 'pink';
+    document.getElementById('footer').style.backgroundColor = 'pink';
+  });
+
+  button2.addEventListener('click', ()=>{
+    document.getElementById('header').style.backgroundColor = 'skyBlue';
+    document.getElementById('footer').style.backgroundColor = 'skyBlue';
+  });
+
+  button3.addEventListener('click', ()=>{
+    document.getElementById('header').style.backgroundColor = 'green';
+    document.getElementById('footer').style.backgroundColor = 'green';
+  });
+
+  button4.addEventListener('click', ()=>{
+    let newPronoun = pronounInput.value;
+    myPronoun.textContent = newPronoun;
+  });
